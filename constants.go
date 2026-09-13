b6248550d92ee065b6190d657597f06a19e133b7
Bulumko <bulumko@racvert.co.za>
2026-09-13 20:46:25 +0200

    feat: add doc info

--- constants.go
+++ constants.go
@@ -1,5 +1,8 @@
 package main
 
+// constants.go
+// This file contains constants used in the application.
+// Create a MySQL database named "learning" and a table named "data" with columns "id" (UUID) and "name" (VARCHAR).
 const (
 	localhost  = "localhost"
 	port       = "5001"
